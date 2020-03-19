dea727ad444b8fb10dc775bcfacefc7b4283973c
blacdoo <brian.lacdoo@ryerson.ca>
2020-03-19 15:38:05 -0400

    Updates

---
+++ precache-manifest.50bf16eaa565b9474b73e49b387a8d92.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971f5435ce1963008e4dd47012116c10",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1b023c18ad5e7c353d11",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a05b5b9c17fd382373ab",
+    "url": "/portfolio/static/css/main.a87f22cc.chunk.css"
+  },
+  {
+    "revision": "1b023c18ad5e7c353d11",
+    "url": "/portfolio/static/js/2.dd1548b7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.dd1548b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05b5b9c17fd382373ab",
+    "url": "/portfolio/static/js/main.a4144150.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "2441709593d05a542e6fdcd089134116",
+    "url": "/portfolio/static/media/clip.24417095.png"
+  },
+  {
+    "revision": "ed994e584b94e418f772fe42f585ef1d",
+    "url": "/portfolio/static/media/linkedin-icon-1.ed994e58.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ef535189cf295e05dfa80fcdafd43e9.js"
+  "/portfolio/precache-manifest.50bf16eaa565b9474b73e49b387a8d92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
